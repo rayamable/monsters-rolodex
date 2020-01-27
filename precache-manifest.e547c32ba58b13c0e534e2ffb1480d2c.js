a96772231709a2b94ceae405e4cce99378fdaf42
Ray Amable <rayamable@gmail.com>
2020-01-27 11:30:03 +0000

    Updates

---
+++ precache-manifest.e547c32ba58b13c0e534e2ffb1480d2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd08fcb7bc490b86906983b19f4f19b1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "7356729684487b9df7e2",
+    "url": "/monsters-rolodex/static/css/main.4e398d8d.chunk.css"
+  },
+  {
+    "revision": "d499d87a993599493776",
+    "url": "/monsters-rolodex/static/js/2.1e4a2dbe.chunk.js"
+  },
+  {
+    "revision": "7356729684487b9df7e2",
+    "url": "/monsters-rolodex/static/js/main.c177de13.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
